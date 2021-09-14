cf428da93ac47f46e983eb657b2c10b857ff03ac
Muhammet Ali Demir <56696870+mhtaldmr@users.noreply.github.com>
2021-09-14 10:32:42 +0300

    Update app.js

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ app.use(express.static("public"));
 
 //connecting mongoose db
 //mongoose.connect("mongodb+srv://malley:kapuska742@cluster0.wk9ld.mongodb.net/todolistDB", {
-mongoose.connect("mongodb+srv://malley:kapuska742@cluster0.wk9ld.mongodb.net/todolistDB",{    
+mongoose.connect("mongodb+srv://malley:kapuska742@cluster0.wk9ld.mongodb.net/todolist",{    
     useNewUrlParser: true,
     useUnifiedTopology: true
 });
